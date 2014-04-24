8d05e955b6b15dc01e1e31512e543312271b0013
Mindaugas - laptop <mseskas@gmail.com>
2014-04-25 00:38:56 +0300

    laptop merge

--- ubuntu -> debian/main v0.1/include/video processing/advanced_opencv.h
+++ ubuntu -> debian/main v0.1/include/video processing/advanced_opencv.h
@@ -1,6 +1,7 @@
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/video/tracking.hpp"
+#include "opencv2/core/core.hpp"
 #include <iostream>
 #include <list>
 #include <vector>
